d62a250f0dc3b86d852bff5a66bf7e8fe21c689c
SaiChand440 <59303987+SaiChand440@users.noreply.github.com>
2021-02-06 11:53:35 +0530

    RecyclerView Added

--- app/src/main/java/com/chandhu/neetprephiringchallenge/MainActivity.kt
+++ app/src/main/java/com/chandhu/neetprephiringchallenge/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.chandhu.neetprephiringchallenge
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.AdapterListUpdateCallback
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.chandhu.neetprephiringchallenge.api.NewsAPI
+import com.chandhu.neetprephiringchallenge.adapters.NewsAdapter
 import com.chandhu.neetprephiringchallenge.api.RetrofitInstance
-import com.chandhu.neetprephiringchallenge.datamodels.NewsResponse
 import com.chandhu.neetprephiringchallenge.utils.Constants.Companion.API_KEY
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var newsAdapter : NewsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,6 +31,12 @@ class MainActivity : AppCompatActivity() {
         val response = RetrofitInstance.api.getNews("in",1, API_KEY)
         if(response.isSuccessful) {
             Log.e("Random",response.body().toString())
+            rvNewsArticles.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                newsAdapter = NewsAdapter()
+                adapter = newsAdapter
+            }
+            newsAdapter.differ.submitList(response.body()?.articles)
         } else {
             Log.e("Random",response.errorBody().toString())
         }
---
+++ app/src/main/java/com/chandhu/neetprephiringchallenge/adapters/NewsAdapter.kt
@@ -0,0 +1,61 @@
+package com.chandhu.neetprephiringchallenge.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.chandhu.neetprephiringchallenge.R
+import com.chandhu.neetprephiringchallenge.datamodels.Article
+import kotlinx.android.synthetic.main.news_item.view.*
+
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    private val differCallBack = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+     val differ = AsyncListDiffer(this, differCallBack)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(news_image)
+            news_title.text = article.title
+            setOnClickListener {
+                onItemClickListener?.let {
+                    it(article)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+}
